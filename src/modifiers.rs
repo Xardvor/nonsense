63cd73431b705cbe1be4f3fdc09cf2dccc21e557
Xardvor <xardvor@gmail.com>
2018-08-31 16:15:22 +0300

    Added article modifier

--- src/modifiers.rs
+++ src/modifiers.rs
@@ -8,6 +8,10 @@ pub struct Modifiers {
 }
 
 impl Modifiers {
+    fn is_vowel(c: char) -> bool {
+        (c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u')
+    }
+
     pub fn create() -> Modifiers {
         let nothing: Processor = |i: &str| -> String {String::from(i)};
 
@@ -19,8 +23,26 @@ impl Modifiers {
             }
         };
 
+        let article: Processor = |input: &str| -> String {
+            let mut iter = input.chars();
+            match iter.next() {
+                None => String::from(input),
+                Some(v) if Modifiers::is_vowel(v) => {
+                    if v == 'u' && input.chars().nth(2) == Some('i') {
+                        return String::from("a ") + input;
+                    };
+
+                    String::from("an ") + input
+                },
+                _ => {
+                    String::from("a ") + input
+                }
+            }
+        };
+
         let mut map: HashMap<String, Processor> = HashMap::new();
         map.insert(String::from("capitalize"), capitalize);
+        map.insert(String::from("a"), article);
 
         Modifiers {
             mods: map,
